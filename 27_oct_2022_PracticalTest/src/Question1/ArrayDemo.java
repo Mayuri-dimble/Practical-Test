0b67eaf3d118ed504dca2629a267542ac7a916d0
Admin <Admin@DESKTOP-VFBAI41>
2022-10-30 11:23:33 +0530

    newly uploaded

--- 27_oct_2022_PracticalTest/src/Question1/ArrayDemo.java
+++ 27_oct_2022_PracticalTest/src/Question1/ArrayDemo.java
@@ -1,14 +1,21 @@
 package Question1;
 
 public class ArrayDemo {
-	void setIntegerarray(int[] array) {
-		for(int i=0;i<array.length;i++) {
-			System.out.println("Array Elements"+i +array[i]);
-			public void manipulateIntegerArray(int arr[]) 
-				for(int i=0;i<arr.length;i++) {
-					System.out.println(arr[i]+5);
-				}
-			}
+	public void manipulateIntegerArray(int arr[])
+	{
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]+5);
+		}
 	}
-				
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]=new int[1];
+		arr[0]=1;
+		ArrayDemo obj=new ArrayDemo();
+		obj.manipulateIntegerArray(arr);
+		
+
+	}
+
 }
